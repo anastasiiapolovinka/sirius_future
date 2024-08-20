4012bc8a2f16bf99263fffc01a016aaae9799fea
Anastasiia Polovynko <nastenka@Nastenkas-MacBook-Air.local>
2024-08-20 17:41:02 +0300

    feat: add g-pages config

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,27 @@ import React from "react";
 import Login from "./login";
 import CalendarLayout from "./calendar";
 import UserLayout from "./editor";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { store } from './store';
-import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { store } from "./store";
+import { Provider } from "react-redux";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/login",
-    element: <Login/>
-  },
-  {
-    path: "/calendar",
-    element: <CalendarLayout/>
-  },
-  {
-    path: "/editor",
-    element: <UserLayout/>
-  },
-]);
-
+const router = createBrowserRouter(
+  [
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/calendar",
+      element: <CalendarLayout />,
+    },
+    {
+      path: "/editor",
+      element: <UserLayout />,
+    },
+  ],
+  { basename: "/sirius_future/" }
+);
 
 function App() {
   return (
